a4ce34a632058ae6adb7a5f32dcd02fc40a0df79
Pierregirard44 <135032423+Pierregirard44@users.noreply.github.com>
2024-07-24 15:31:18 +0200

    login and creat login WIP

--- frontRivendell/src/app/pages/login/login.component.ts
+++ frontRivendell/src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,20 +9,34 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
+  profileCreated: boolean = false;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       username: [''],
       password: ['']
     });
+
+    this.route.queryParams.subscribe(params => {
+      if (params['profileCreated']) {
+        this.profileCreated = true;
+      }
+    });
   }
 
   onSubmit(): void {
     if (this.loginForm.valid) {
-      console.log('Username:', this.loginForm.get('username')?.value);
-      console.log('Password:', this.loginForm.get('password')?.value);
+      // Convert form values to JSON and log it
+      console.log(JSON.stringify(this.loginForm.value));
+      
+      // Optionally, redirect to another page if needed
+      // this.router.navigate(['/some-route']);
     }
   }
 }
