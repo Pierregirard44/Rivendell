a0c86c569097e567d166c4bf8b516a9b56fb8309
Bradley Hill <brad.michael.hill@gmail.com>
2024-07-24 12:14:56 +0200

    added insertion of mock data into tables

---
+++ backRivendell/demo/src/main/resources/db/migration/V5__Insert_mock_data_into_events.sql
@@ -0,0 +1,6 @@
+INSERT INTO events (name, description, date, created_by) VALUES
+                                                             ('Event 1', 'Description for Event 1', '2023-10-01 10:00:00', 1),
+                                                             ('Event 2', 'Description for Event 2', '2023-10-15 14:00:00', 2),
+                                                             ('Event 3', 'Description for Event 3', '2023-11-01 09:00:00', 3),
+                                                             ('Event 4', 'Description for Event 4', '2023-11-10 11:00:00', 4),
+                                                             ('Event 5', 'Description for Event 5', '2023-12-01 15:00:00', 5);
