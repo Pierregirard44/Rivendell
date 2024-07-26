a15564c9867857d74ae5782b467b656050ccebe8
Pierregirard44 <135032423+Pierregirard44@users.noreply.github.com>
2024-07-26 12:09:32 +0200

    event card modify to test

--- frontRivendell/src/app/pages/events/events.component.ts
+++ frontRivendell/src/app/pages/events/events.component.ts
@@ -16,13 +16,60 @@ export class EventsComponent implements OnInit {
   }
 
   loadEvents(): void {
-    this.eventService.getEvents().subscribe(
-      (data) => {
-        this.events = data;
-      },
-      (error) => {
-        console.error('Erreur lors du chargement des événements:', error);
-      }
-    );
+      this.events = [
+        {
+          id: 1,
+          title: 'Soirée jeux de société',
+          date: '2021-08-15',
+          location: 'Rivendell',
+          description: 'Venez nombreux pour une soirée jeux de société !',
+          image: 'https://via.placeholder.com/150',
+          participant: 'Pierre et Brad',
+        },
+        {
+          id: 2,
+          title: 'Soirée jeux de société',
+          date: '2021-08-15',
+          location: 'Rivendell',
+          description: 'Venez nombreux pour une soirée jeux de société !',
+          image: 'https://via.placeholder.com/150',
+          participant: 'Pierre et Brad',
+        },
+        {
+          id: 3,
+          title: 'Soirée jeux de société',
+          date: '2021-08-15',
+          location: 'Rivendell',
+          description: 'Venez nombreux pour une soirée jeux de société !',
+          image: 'https://via.placeholder.com/150',
+          participant: 'Pierre et Brad',
+        },
+        {
+          id: 4,
+          title: 'Soirée jeux de société',
+          date: '2021-08-15',
+          location: 'Rivendell',
+          description: 'Venez nombreux pour une soirée jeux de société !',
+          image: 'https://via.placeholder.com/150',
+          participant: 'Pierre et Brad',
+        },
+        {
+          id: 5,
+          title: 'Soirée jeux de société',
+          date: '2021-08-15',
+          location: 'Rivendell',
+          description: 'Venez nombreux pour une soirée jeux de société !',
+          image: 'https://via.placeholder.com/150',
+          participant: 'Pierre et Brad',
+        }
+      ];
+    // this.eventService.getEvents().subscribe(
+    //   (data) => {
+    //     this.events = data;
+    //   },
+    //   (error) => {
+    //     console.error('Erreur lors du chargement des événements:', error);
+    //   }
+    // );
   }
 }
